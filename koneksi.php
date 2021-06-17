5122cf6274546599646e96fe6ed2e431d79a4ded
Aririzqi13 <aririzqi13@gmail.com>
2021-06-17 14:26:01 +0700

    coming soon
    sabar

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,1 @@
-<?php
-	
-	    $server = "localhost";
-	    $user = "root";
-	    $pass = "root";
-	    $db = "wsmhs";
-	
-	    $kon = mysqli_connect($server, $user, $pass, $db);
-	    if (!$kon) {
-	        die("gagal!"); 
-	    } 
-	
-	?>
+kode akan di push nanti :)
