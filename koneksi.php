c4161f6ca6dcc59a515d13960922af123d3aab61
Aririzqi13 <aririzqi13@gmail.com>
2021-06-17 14:30:02 +0700

    Latihan rest server api
    Latihan rest server api perkuliahan daring

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,13 @@
-kode akan di push nanti :)
+<?php
+	
+	    $server = "localhost";
+	    $user = "root";
+	    $pass = "root";
+	    $db = "wsmhs";
+	
+	    $kon = mysqli_connect($server, $user, $pass, $db);
+	    if (!$kon) {
+	        die("gagal!"); 
+	    } 
+	
+	?>
